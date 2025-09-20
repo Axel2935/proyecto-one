27b2caac29b95eea417a0a73a68241a958430244
Axel Reyes Prado <axelgrepra@gmail.com>
2025-09-20 18:47:37 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function mostrarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    amigos.push(nombre);
+    input.value = "";
+    mostrarLista();
+}
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 Tu amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
